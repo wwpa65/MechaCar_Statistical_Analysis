f741040d254ae58aac78fbf0493c82ad92bf6f08
Wimal Pathmasiri <wimal@Wimals-MBP.lan>
2022-06-19 21:39:41 -0400

    Added files

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,4 +1,7 @@
+# DELIVERABLE 1
+#Load rge dplyr library
 library(dplyr)
+
 # Import MechaCar_mpg.csv file
 MechaCar <- read.csv(file="MechaCar_mpg.csv")
 
@@ -8,17 +11,18 @@ reg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearan
 #Summary statistics of linear regression
 summary(reg)
 
+#DELIVERABLE 2
 # Import Suspension_Coil.csv file
 Suspension_Coil <- read.csv(file="Suspension_Coil.csv")
 
 #Summarize statisticas
-
 lot <- group_by(Suspension_Coil, Manufacturing_Lot)
 
 total_summary <- summarize(lot, Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 
+#DELIVERABLE 3
 # ttest for all to population mean of 1500
-t.test(lot, mu=1500.00)
+t.test(Suspension_Coil['PSI'], mu=1500.00)
 
 
 # ttest for individual lot to population mean
@@ -27,12 +31,13 @@ lot2 <- subset(Suspension_Coil, Manufacturing_Lot == "Lot2")
 lot3 <- subset(Suspension_Coil, Manufacturing_Lot == "Lot3")
 
 t.test(lot1["PSI"], mu=1500)
-t.test(lot1["PSI"], mu=1500)
-t.test(lot1["PSI"], mu=1500)
+t.test(lot2["PSI"], mu=1500)
+t.test(lot3["PSI"], mu=1500)
 
+#===========================================
 #Alternatively in a single line for each lot
 
 t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot1")["PSI"], mu=1500)
 t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot2")["PSI"], mu=1500)
 t.test(subset(Suspension_Coil, Manufacturing_Lot == "Lot3")["PSI"], mu=1500)
-
+#===========================================
